57bd3f281e7f2cc3bd3e3c2857eeee6c3e62a1f0
“ayush <ayushsaklani04@gmail.com>
2024-03-28 23:04:18 +0530

    improv

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,15 +38,7 @@
     });
   };
 
-  /**
-   * Mobile nav toggle
-   */
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("#navbar").classList.toggle("navbar-mobile");
-    this.classList.toggle("bi-list");
-    this.classList.toggle("bi-x");
-  });
-
+ 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
