617d277e8dcc0da598a66a2b4c7b28dc4bd9d575
“ayush <ayushsaklani04@gmail.com>
2024-03-30 11:06:09 +0530

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,28 +14,30 @@ on("click", "#navbar .nav-link", function (e) {
 	let section = select(this.hash);
 	console.log(this.hash)
 	if (section) {
-		let header = select("#header", false),
-		sections = select("section", true),
-		navlinks = select("#navbar .nav-link", true);
-		console.log(sections)
+		let header = select("#header", false);
+		let sections = select("section", true);
+		let navlinks = select("#navbar .nav-link", true);
+		// console.log("1 "+header)
+		// console.log("2 "+sections)
+		// console.log("3 "+navlinks)
 
 		navlinks.forEach((item) => item.classList.remove("active"));
 		this.classList.add("active");
 
 		if (this.hash == "#header") {
 			header.classList.remove("header-top");
-			sections.forEach((item) => item.classList.remove("section-show"));
+			sections.forEach((item) => item.classList.remove("section-overlay"));
 			return;
 		}
 		if (!header.classList.contains("header-top")) {
 			header.classList.add("header-top");
 			setTimeout(() => {
-				sections.forEach((item) => item.classList.remove("section-show"));
-				section.classList.add("section-show");
+				sections.forEach((item) => item.classList.remove("section-overlay"));
+				section.classList.add("section-overlay");
 			}, 500);
 		} else {
-			sections.forEach((item) => item.classList.remove("section-show"));
-			section.classList.add("section-show");
+			sections.forEach((item) => item.classList.remove("section-overlay"));
+			section.classList.add("section-overlay");
 		}
 		section.scrollIntoView({ behavior: "smooth" });
 	}
